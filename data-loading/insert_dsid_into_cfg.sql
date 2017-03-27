916956688f0959eef4c24aedf4166e77e25fbd8c
cbode <collin@berkeley.edu>
2017-03-27 14:19:28 -0700

    Angelo Inserts
    aggregates for datavalues2 table

--- data-loading/insert_dsid_into_cfg.sql
+++ data-loading/insert_dsid_into_cfg.sql
@@ -1,4 +1,6 @@
 -- Insert DatastreamID's into cfg tables
+
+-- UCNRS
 -- Precipitation Daily Sum
 INSERT INTO cfg_load_datavalues_day_sum (DatastreamID) 
 SELECT 
@@ -53,3 +55,99 @@ SELECT DatastreamID FROM datastreams
 WHERE 
 datastreamname like 'Soil Temp Deg C % Avg' AND 
 MC_Name = "UCNRS";
+
+
+--
+-- Angelo
+-- 
+-- cfg_load_datavalues_day_sum
+
+-- Precipitation
+INSERT INTO cfg_load_datavalues_day_sum (DatastreamID) 
+SELECT DatastreamID FROM datastreams 
+WHERE 
+variablecode like 'Rainfall mm' 
+AND datastreamname not like '%30min'  
+AND MC_Name = "Angelo Reserve";
+
+
+-- cfg_load_datavalues_day_mma
+
+-- Well Water Levels
+INSERT INTO cfg_load_datavalues_day_mma (DatastreamID) 
+SELECT datastreamid, datastreamname FROM datastreams 
+WHERE variablecode LIKE 'Water Level m' 
+AND datastreamname NOT LIKE '%Old' 
+AND datastreamname not like '%30min';
+
+-- Air Temperature
+INSERT INTO cfg_load_datavalues_day_mma (DatastreamID) 
+SELECT DatastreamID,datastreamname FROM datastreams 
+WHERE 
+MC_Name = "Angelo Reserve" 
+AND VariableCode like 'Air Temp C' 
+AND datastreamname not like '%30min' 
+AND datastreamname not like '%Delta%' 
+AND datastreamname not like '%Min' 
+AND datastreamname not like '%Max';
+
+-- Wind Speed
+INSERT INTO cfg_load_datavalues_day_mma (DatastreamID) 
+SELECT DatastreamID FROM datastreams 
+WHERE 
+MC_Name = "Angelo Reserve" 
+AND VariableCode like  '%wind speed%ms'  
+AND datastreamname not like '%30min' 
+AND  datastreamname not like '%ingrid%';
+
+
+-- Battery Voltage 
+INSERT INTO cfg_load_datavalues_day_mma (DatastreamID) 
+SELECT DatastreamID FROM datastreams 
+WHERE 
+MC_Name = "Angelo Reserve" 
+AND VariableCode = "Battery Voltage" 
+AND datastreamname not like '%_2' 
+AND datastreamname not like '%Avg' 
+AND datastreamname not like '%Max';
+
+
+-- Relative Humidity
+INSERT INTO cfg_load_datavalues_day_mma (DatastreamID) 
+SELECT DatastreamID FROM datastreams 
+WHERE 
+MC_Name = "Angelo Reserve" 
+AND VariableCode like  'Rel Humidity Perc'  
+AND datastreamname not like '%30min' 
+AND  datastreamname not like '%ingrid%';
+
+
+-- Barometric Pressure
+INSERT INTO cfg_load_datavalues_day_mma (DatastreamID) 
+SELECT DatastreamID FROM datastreams 
+WHERE 
+MC_Name = "Angelo Reserve" 
+AND VariableCode like  'Barometric Pressure mb'  
+AND datastreamname not like '%30min' 
+AND  datastreamname not like '%ingrid%';
+
+
+-- Soil Temperature <-- this is not used in the dashboard
+-- 80 DSID's if all are allowed.  22 DSID's if 4 inch depth is used.
+-- 63 rows used
+INSERT INTO cfg_load_datavalues_day_mma (DatastreamID) 
+SELECT DatastreamID FROM datastreams 
+WHERE 
+MC_Name = "Angelo Reserve" 
+AND VariableCode like  'Soil Temperature C'  
+AND datastreamname not like '%30min' 
+AND datastreamname not like '%Max' 
+AND datastreamname not like '%Min';
+
+
+-- Soil Moisture TDR only
+INSERT INTO cfg_load_datavalues_day_mma (DatastreamID) 
+SELECT DatastreamID FROM datastreams 
+WHERE 
+MC_Name = "Angelo Reserve" 
+AND VariableCode like 'Soil Moisture Pct%';  
