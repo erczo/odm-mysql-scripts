af0f8a75a7cecb86cd331cdafca0029dcc27b0b8
cbode <collin@berkeley.edu>
2017-03-28 17:02:49 -0700

    Perl cron job scripts added
    Two scripts to be placed on cron job for hourly and daily rollup
    functions.

---
+++ snippets/odm_procedures_daily.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+################################################################
+# ODM Procedures Hourly
+# @author: Scott Smith, Collin Bode
+# @date:  2017-03-28
+#
+# Purpose: simple launch of the hourly aggregation functions.
+#
+################################################################ 
+use DBI;
+
+# Connect to database
+$dbh = DBI->connect("dbi:mysql:$database:gall.bnhm.berkeley.edu","collin","Kanalani99");
+if ( !defined $dbh ) { die "Cannot connect to mysql server: $DBI::errstr\n"; }
+
+# run 
+$query = "call usp_run_daily";
+$sth = $dbh->prepare( $query ) or die "Can't prepare statement: $DBI::errstr\n";
+$sth->execute;
+
+# clean up
+$sth->finish;
+$dbh->disconnect;
